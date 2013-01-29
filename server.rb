1281ffa31f9614b5f8478a6727a7be8e3f29f80f
Dag-Inge Aas <daginge@gmail.com>
2013-01-29 12:06:06 +0100

    update cache

--- server.rb
+++ server.rb
@@ -3,8 +3,7 @@ require 'sinatra'
 require 'open-uri'
 require 'logger' 
 
-@@cache_timer = Time.now - 30*60
-@@respons = ""
+@@cache = {"123" => "test"}
 
 configure do 
   Log = Logger.new("sinatra.log")
@@ -16,14 +15,12 @@ get "/" do
 end
 
 get "/statuses/oembed/:tweet" do 
-  if Time.now > @@cache_timer + 15*60
+  if not @@cache[params[:tweet]]
     url = "http://api.twitter.com/1/statuses/oembed.json?id=" + params[:tweet]
-    Log.info url
     open(URI(url)) do |http|
-      @@respons = http.read
+       @@cache[params[:tweet]] = http.read
     end
-    @@cache_timer = Time.now
   end
 
-  return @@respons
+  return @@cache[params[:tweet]]
 end
